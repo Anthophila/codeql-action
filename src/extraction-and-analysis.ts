511bf52ef0a2477d105e58f28981d810e813eeb5
Ana Armas Romero <54946499+anaarmas@users.noreply.github.com>
2019-12-06 11:38:34 +0000

    Update extraction-and-analysis.ts
    updated extraction-analysis.ts

--- src/extraction-and-analysis.ts
+++ src/extraction-and-analysis.ts
@@ -1,27 +1,50 @@
 import * as core from '@actions/core';
-import * as toolcache from '@actions/tool-cache';
 import * as exec from '@actions/exec';
-import * as io from '@actions/io';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as setuptools from './setup-tools';
 
 async function run() {
   try {
 
+    core.startGroup('Setup CodeQL tools');
+    const codeqlSetup = await setuptools.setupCodeQL();
     const language = core.getInput('language', { required: true });
+    core.endGroup();
+
+    core.startGroup('Search for auxiliary build file');
+    const databasesFolder = path.resolve("codeql_dbs");
+    const languageDatabase = path.join(databasesFolder, language);
+    let extractionCall = ['database', 'create', languageDatabase,
+                '--language='+language ];
+    const buildFilePath = findAuxBuildFile(language);
+    if (buildFilePath)
+      extractionCall.concat(['--command='+buildFilePath]);
+    core.endGroup();
     
+    core.startGroup('Create database');
+    await exec.exec(codeqlSetup.cmd, extractionCall);
+    core.endGroup();
+
+    core.startGroup('Run analysis');
+    const sarifFolder = path.resolve("codeql_alerts");
+    await exec.exec(codeqlSetup.cmd, ['database', 'analyze', languageDatabase, 
+                                    '--format=sarif-latest',
+                                    '--output=' + path.join(sarifFolder, languageDatabase + '.sarif'),
+                                    language + '-lgtm.qls']);
+    core.endGroup();
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+function findAuxBuildFile(language : string) : string|undefined {
+  try {
     const ext = process.platform == 'win32' ? '.cmd' : '.sh';
     const buildFilePath = path.resolve('codeql-build-' + language + ext);
-    let call = ['database', 'create', path.resolve("codeql_db_"+language),
-                '--language='+language ];
-                
     if (fs.existsSync(buildFilePath))
-        call.concat(['--command='+buildFilePath]);
-    
-    await exec.exec('codeql', call);
-
-    //TODO run the analysis on the generated database!
-    
+      return buildFilePath;
   } catch (error) {
     core.setFailed(error.message);
   }

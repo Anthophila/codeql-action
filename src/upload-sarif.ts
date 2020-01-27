314a759e91397162980c53ce308ff2ccbc25215a
David Verdeguer <david@semmle.com>
2020-01-27 14:40:56 +0100

    Read necessary environment params

--- src/upload-sarif.ts
+++ src/upload-sarif.ts
@@ -2,6 +2,17 @@ import * as core from '@actions/core';
 
 async function run() {
     let location = core.getInput('location', {required: true});
+
+    const commitOid = process.env['GITHUB_SHA'];
+    // Its in the form of 'refs/heads/master'
+    let branchName = process.env['GITHUB_REF'];
+    // RepoID seems to not be available, using runid as a placeholder
+    // Repo name avaliable under GITHUB_REPOSITORY
+    let repoID = process.env['GITHUB_RUN_ID'];
+
+    let analysisName = process.env['GITHUB_WORKFLOW'];
+    
+    
 }
 
 run();

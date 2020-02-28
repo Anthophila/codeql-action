9c2506cd7eb803a726c6b493697ce18af4efd70e
Robert Brignull <robert@semmle.com>
2020-02-28 16:17:41 +0000

    update whitespace

--- src/setup-tools.ts
+++ src/setup-tools.ts
@@ -10,27 +10,27 @@ export class CodeQLSetup {
     cmd: string;
     platform: string;
 
-    constructor(codeqlDist : string) {
+    constructor(codeqlDist: string) {
         this.dist = codeqlDist;
         this.tools = path.join(this.dist, 'tools');
         this.cmd = path.join(codeqlDist, 'codeql');
         // TODO check process.arch ?
         if (process.platform == 'win32') {
-           this.platform = 'win64';
-           if (this.cmd.endsWith('codeql')) {
-               this.cmd += ".cmd";
-           }
+            this.platform = 'win64';
+            if (this.cmd.endsWith('codeql')) {
+                this.cmd += ".cmd";
+            }
         } else if (process.platform == 'linux') {
-           this.platform = 'linux64';
+            this.platform = 'linux64';
         } else if (process.platform == 'darwin') {
-           this.platform = 'osx64';
+            this.platform = 'osx64';
         } else {
-           throw new Error("Unsupported plaform: " + process.platform);
+            throw new Error("Unsupported plaform: " + process.platform);
         }
     }
 }
 
-export async function setupCodeQL() : Promise<CodeQLSetup> {
+export async function setupCodeQL(): Promise<CodeQLSetup> {
     const version = '1.0.0';
     const codeqlURL = core.getInput('tools', { required: true });
 

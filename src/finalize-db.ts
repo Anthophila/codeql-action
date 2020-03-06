8f4bcc839431d07f24b890b69d9233b3187a2a95
Ana Armas Romero <54946499+anaarmas@users.noreply.github.com>
2020-03-06 14:54:41 +0100

    update default query suite

--- src/finalize-db.ts
+++ src/finalize-db.ts
@@ -78,11 +78,16 @@ async function runQueries(codeqlCmd: string, resultsFolder: string, config: conf
     core.startGroup('Analyzing ' + database);
 
     const sarifFile = path.join(sarifFolder, database + '.sarif');
-    await exec.exec(codeqlCmd, ['database', 'analyze', path.join(databaseFolder, database),
-      '--format=sarif-latest', '--output=' + sarifFile,
+    await exec.exec(codeqlCmd, [
+      'database',
+      'analyze',
+      path.join(databaseFolder, database),
+      '--format=sarif-latest',
+      '--output=' + sarifFile,
       '--no-sarif-add-snippets',
-      database + '-lgtm.qls',
-      ...config.inRepoQueries]);
+      database + '.qls',
+      ...config.inRepoQueries,
+    ]);
 
     let sarifObject = JSON.parse(fs.readFileSync(sarifFile, 'utf8'));
     appendSarifRuns(combinedSarif, sarifObject);
